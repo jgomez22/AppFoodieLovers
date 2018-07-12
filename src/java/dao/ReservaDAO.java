9a5af3435a2245f801b75c5e183fda8c41ab1da3
jgomez22 <josuegomez1994@hotmail.cocribm>
2018-07-12 16:45:03 -0500

    diseño

--- src/java/dao/ReservaDAO.java
+++ src/java/dao/ReservaDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
 import java.sql.PreparedStatement;
@@ -14,6 +9,89 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import modelo.Reserva;
 
-public class ReservaDAO {
-    
+public class ReservaDAO extends dao {
+
+    public void actualizaraPro(int idres, int nest) throws SQLException {
+        try {
+            this.Conectar();
+            PreparedStatement pst = this.getCn().prepareStatement("UPDATE reserva SET idestado=? WHERE idreserva=?");
+            pst.setInt(1, nest);
+            pst.setInt(2, idres);
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ReservaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public List<Reserva> listarEmpresa(int id, int idest) throws SQLException {
+        List<Reserva> lista = null;
+        ResultSet rs;
+        try {
+            this.Conectar();
+            PreparedStatement pst;
+            pst = this.getCn().prepareStatement(
+                    "SELECT r.idreserva idreserva, r.cantidad cantidad, r.hora_llegada hora_llegada, r.importe_total importe_total, r.detalles detalles, r.idusuario idusuario, r.idproducto idproducto, r.idestado idestado "
+                    + "FROM reserva r, producto p, empresa em, usuario u "
+                    + "WHERE r.idproducto=p.idproducto AND p.idempresa=em.idempresa AND em.idusuario=u.idusuario AND u.idusuario=? AND r.idestado=?");
+            pst.setInt(1, id);
+            pst.setInt(2, idest);
+            rs = pst.executeQuery();
+            lista = new ArrayList();
+
+            while (rs.next()) {
+                Reserva res = new Reserva();
+                res.setIdreserva(rs.getInt("idreserva"));
+                res.setCantidad(rs.getInt("cantidad"));
+                res.setHora_llegada(rs.getString("hora_llegada"));
+                res.setImporte_total(rs.getDouble("importe_total"));
+                res.setDetalles(rs.getString("detalles"));
+                res.setIdusuario(rs.getInt("idusuario"));
+                res.setIdproducto(rs.getInt("idproducto"));
+                res.setIdestado(rs.getInt("idestado"));
+                lista.add(res);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ReservaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            this.Cerrar();
+        }
+        return lista;
+    }
+
+    public List<Reserva> listar(int id) throws SQLException {
+        List<Reserva> lista = null;
+        ResultSet rs;
+        try {
+            this.Conectar();
+            PreparedStatement pst = this.getCn().prepareStatement("SELECT "
+                    + "idreserva, cantidad, importe_total, direccion, detalles, idusuario, idproducto, idestado "
+                    + "FROM reserva "
+                    + "WHERE idusuario=?");
+            pst.setInt(1, id);
+            rs = pst.executeQuery();
+            lista = new ArrayList();
+
+            while (rs.next()) {
+                Reserva res = new Reserva();
+                res.setIdreserva(rs.getInt("idreserva"));
+                res.setCantidad(rs.getInt("cantidad"));
+                res.setHora_llegada(rs.getString("hora_llegada"));
+                res.setImporte_total(rs.getDouble("importe_total"));
+                res.setDetalles(rs.getString("detalles"));
+                res.setIdusuario(rs.getInt("idusuario"));
+                res.setIdproducto(rs.getInt("idproducto"));
+                res.setIdestado(rs.getInt("idestado"));
+                lista.add(res);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ReservaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            this.Cerrar();
+        }
+        return lista;
+    }
 }
